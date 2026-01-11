ec6ce1afa02b0fd1c80ace47eeec8f13b41df091
root <root@DESKTOP-67JC1MP.localdomain>
2026-01-11 04:20:54 +0300

    wtf

--- src/ts/endGame.ts
+++ src/ts/endGame.ts
@@ -2,9 +2,17 @@ import { addClass } from "../utils"
 
 
 
+function getCpm() {
+  const allCorrect = document.querySelectorAll("#words .letter.correct")
+  const cpm = allCorrect.length * 2
+  return cpm
+}
+
 export function endGame() {
   if (window.timer) clearInterval(window.timer)
   const gameEl = document.getElementById('game')
   if (!gameEl) return
   addClass(gameEl, 'over')
+  console.log("game over, number of correct letters:", getCpm())
+  document.querySelector("#result").textContent = `CPM: ${getCpm()}`
 }
